1dfd4ce93f27a8937341d0b112c44b62f3544966
NeonGooRoo <neongooroo@gmail.com>
2024-03-12 15:14:13 +0300

    2.0

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,40 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { hopeTheme } from "vuepress-theme-hope";
+
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/en/": {
+      lang: "en-US",
+      title: "NeonGooRoo's Guide",
+      description: "Japanese with fun and ease",
+    },
+
+    "/": {
+      lang: "ru-RU",
+      title: "NeonGoo.Ru",
+      description: "Японский легко и с удовольствием",
+    },
+
+  },
+
+  plugins: [
+      googleAnalyticsPlugin({
+        id: 'G-G6HDTB0VXG',
+      }),
+  ],
+
+  theme,
+
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
+
+
+
+
---
+++ src/.vuepress/theme.ts
@@ -0,0 +1,248 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import { enNavbar, ruNavbar } from "./navbar/index.js";
+import { enSidebar, ruSidebar } from "./sidebar/index.js";
+
+
+
+export default hopeTheme({
+  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+
+  author: {
+    name: "NeonGooRoo",
+    url: "https://t.me/neongooroo",
+  },
+
+
+
+  iconAssets: "fontawesome-with-brands",
+
+
+  logo: "/ico/logo.png",
+
+  repo: "neongooroo/neongooru",
+
+
+  docsDir: "src",
+
+  print: false,
+
+
+
+  locales: {
+    "/en/": {
+
+      navbar: enNavbar,
+
+      sidebar: enSidebar,
+
+      footer: "Default footer",
+
+      displayFooter: true,
+
+      metaLocales: {
+        editLink: "Edit this page on GitHub",
+      },
+    },
+
+
+
+    "/": {
+
+      navbar: ruNavbar,
+
+      sidebar: ruSidebar,
+
+      footer: "bebra",
+
+      displayFooter: true,
+
+      metaLocales: {
+        editLink: "在 GitHub 上编辑此页",
+      },
+    },
+  },
+
+  encrypt: {
+    config: {
+      "/demo/encrypt.html": ["1234"],
+      "/en/demo/encrypt.html": ["1234"],
+    },
+  },
+
+  plugins: {
+    // You should generate and use your own comment service
+    blog: true,
+
+    docsearch: {
+      appId: "",
+      apiKey: "",
+      indexName: "",
+      locales: {
+        '/': {
+          placeholder: '',
+          translations: {
+            button: {
+              buttonText: '',
+            },
+          },
+        },
+        '/en/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: 'Search Documentation',
+            },
+          },
+        },
+      },
+    },
+
+    copyright: {
+      global: true,
+      author: "NeonGooRoo",
+      license: "MIT",
+
+    },
+    comment: {
+      provider: "Giscus",
+      repo: "neongooroo/neongooru",
+      repoId: "R_kgDOLRM99A",
+      category: "General",
+      categoryId: "DIC_kwDOLRM99M4Cdyoi",
+      
+    },
+
+    components: {
+      components: ["Badge", "VPCard", "Share",
+      "VidStack",],
+    },
+
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      codetabs: true,
+      component: true,
+      demo: true,
+      figure: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      mark: true,
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+      
+
+      revealJs: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+        },
+
+
+      // install chart.js before enabling it
+      // chart: true,
+
+      // insert component easily
+
+      // install echarts before enabling it
+      // echarts: true,
+
+      // install flowchart.ts before enabling it
+      // flowchart: true,
+
+      // gfm requires mathjax-full to provide tex support
+      // gfm: true,
+
+      // install katex before enabling it
+      // katex: true,
+
+      // install mathjax-full before enabling it
+      // mathjax: true,
+
+      // install mermaid before enabling it
+      // mermaid: true,
+
+      // playground: {
+      //   presets: ["ts", "vue"],
+      // },
+
+
+
+      // install @vue/repl before enabling it
+      // vuePlayground: true,
+
+      // install sandpack-vue3 before enabling it
+      // sandpack: true,
+    },
+
+    // install @vuepress/plugin-pwa and uncomment these if you want a PWA
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
